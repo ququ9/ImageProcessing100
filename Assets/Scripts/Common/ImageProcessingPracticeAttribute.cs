a68893b2097fabd24831281a7b52bfe96ca6dc38
ququ9 <35394815+ququ9@users.noreply.github.com>
2019-12-30 03:34:59 +0900

    独自Attribute

---
+++ Assets/Scripts/Common/ImageProcessingPracticeAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+public class ImageProcessingPracticeAttribute : Attribute
+{
+    public int Number { get; }
+    public string Description { get; }
+
+    public ImageProcessingPracticeAttribute(int number, string description = null)
+    {
+        this.Number = number;
+
+        if (string.IsNullOrEmpty(description)) {
+            this.Description = this.Number.ToString();
+        } else {
+            this.Description = $"{this.Number} {description}";
+        }
+    }
+}
