7fcd0144da5d819d7cd6930b1a5fecdee0a725d3
ququ9 <35394815+ququ9@users.noreply.github.com>
2019-12-30 03:35:43 +0900

    実際に画像処理を行うクラスのベース

---
+++ Assets/Scripts/Common/ImageProcessingPractice.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public abstract class ImageProcessingPractice : MonoBehaviour
+{
+    protected Texture2D _result;
+
+    public Texture2D ResultTexture => _result;
+
+    public virtual void CreateResultTexture(Texture2D source)
+    {
+        _result = new Texture2D(source.width, source.height, source.format, false, false);
+    }
+
+    public abstract void OnProcess(TextureData source);
+}
