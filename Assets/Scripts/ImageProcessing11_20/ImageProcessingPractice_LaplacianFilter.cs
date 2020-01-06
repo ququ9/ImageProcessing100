9cda16d1c40479b797e7e69adf697f71d87983af
ququ9 <35394815+ququ9@users.noreply.github.com>
2020-01-07 01:48:13 +0900

    Laplacianフィルタ

---
+++ Assets/Scripts/ImageProcessing11_20/ImageProcessingPractice_LaplacianFilter.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using System.Text;
+using UnityEngine;
+using Unity.Jobs;
+
+[ImageProcessingPractice(17, "Laplacianフィルター")]
+public class ImageProcessingPractice_LaplacianFilter : ImageProcessingPractice
+{
+    public override void OnProcess()
+    {
+        var kernel = new ConvolutionKernel("0,1,0\n1,-4,1\n0,1,0");
+
+        using (var grayScale = TextureData.CreateGrayScaleFromRGB(_source, isTemporal: false)) {
+            var job = ApplyKernelJob.Create(grayScale, kernel);
+            var handle = job.Schedule(job.Result.Length, 8);
+            handle.Complete();
+        
+            using (var temp = new TextureData(_source.Width, _source.Height, job.Result)) {
+                temp.ApplyToTexture(_result);
+            }
+        }
+    }
+}
