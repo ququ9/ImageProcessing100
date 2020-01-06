e86106a1d2ed90ab20dcf6eeb878c6c0582571fa
ququ9 <35394815+ququ9@users.noreply.github.com>
2020-01-06 22:43:34 +0900

    Job汎用化

---
+++ Assets/Scripts/Common/ApplyKernelJob.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections;
+using Unity.Jobs;
+using UnityEngine;
+
+public struct ApplyKernelJob : IJobParallelFor
+{
+    [ReadOnly] public NativeArray<Color32> Pixels;
+
+    [ReadOnly, DeallocateOnJobCompletion] public NativeArray<float> Kernel;
+
+    [WriteOnly] public NativeArray<Color32> Result;
+
+    [ReadOnly] public int KernelSize;
+    [ReadOnly] public int Width;
+    [ReadOnly] public int Height;
+
+    public void Execute(int index)
+    {
+        var pixelY = (index / this.Width);
+        var pixelX = (index - (pixelY * this.Width));
+        var kernelCenter = this.KernelSize / 2;
+
+        float r = 0.0f, g = 0.0f, b = 0.0f;
+        for (var iKernel = 0; iKernel < Kernel.Length; ++iKernel) {
+            var weight = this.Kernel[iKernel];
+
+            var yKernel = (iKernel / KernelSize);
+            var xKernel = (iKernel - (yKernel * KernelSize));
+
+            var x = Mathf.Clamp(pixelX + xKernel - kernelCenter, 0, this.Width - 1);
+            var y = Mathf.Clamp(pixelY + yKernel - kernelCenter, 0, this.Height - 1);
+            var c = this.Pixels[(y * this.Width) + x];
+            r += (weight * c.r);
+            g += (weight * c.g);
+            b += (weight * c.b);
+        }
+
+        var pixelIndex = ((pixelY * this.Width) + pixelX);
+        var o = this.Pixels[pixelIndex];
+        this.Result[pixelIndex] = new Color32((byte)Mathf.Clamp((int)r, 0, 255) , (byte)Mathf.Clamp((int)g, 0, 255), (byte)Mathf.Clamp((int)b, 0, 255), o.a);
+    }
+}
--- Assets/Scripts/ImageProcessing0_10/ImageProcessingPractice_GaussianFilter.cs
+++ Assets/Scripts/ImageProcessing0_10/ImageProcessingPractice_GaussianFilter.cs
@@ -14,7 +14,7 @@ public class ImageProcessingPractice_GaussianFilter : ImageProcessingPractice
         var variance = (_standardDeviation * _standardDeviation);
         var kernel = this.CreateGaussianKernel(_kernelSize, variance);
 
-        var job = new ImageProcessJob
+        var job = new ApplyKernelJob
         {
           Pixels = _source.Pixels,
           Width = _source.Width,
@@ -52,44 +52,5 @@ public class ImageProcessingPractice_GaussianFilter : ImageProcessingPractice
         }
 
         return kernel;
-    }
-
-    private struct ImageProcessJob : IJobParallelFor
-    {
-        [ReadOnly] public NativeArray<Color32> Pixels;
-
-        [ReadOnly, DeallocateOnJobCompletion] public NativeArray<float> Kernel;
-
-        [WriteOnly] public NativeArray<Color32> Result;
-
-        [ReadOnly] public int KernelSize;
-        [ReadOnly] public int Width;
-        [ReadOnly] public int Height;
-
-        public void Execute(int index)
-        {
-            var pixelY = (index / this.Width);
-            var pixelX = (index - (pixelY * this.Width));
-            var kernelCenter = this.KernelSize / 2;
-
-            float r = 0.0f, g = 0.0f, b = 0.0f;
-            for (var iKernel = 0; iKernel < Kernel.Length; ++iKernel) {
-                var weight = this.Kernel[iKernel];
-
-                var yKernel = (iKernel / KernelSize);
-                var xKernel = (iKernel - (yKernel * KernelSize));
-
-                var x = Mathf.Clamp(pixelX + xKernel - kernelCenter, 0, this.Width - 1);
-                var y = Mathf.Clamp(pixelY + yKernel - kernelCenter, 0, this.Height - 1);
-                var c = this.Pixels[(y * this.Width) + x];
-                r += (weight * c.r);
-                g += (weight * c.g);
-                b += (weight * c.b);
-            }
-
-            var pixelIndex = ((pixelY * this.Width) + pixelX);
-            var o = this.Pixels[pixelIndex];
-            this.Result[pixelIndex] = new Color32((byte)Mathf.Clamp((byte)r, 0, 255) , (byte)Mathf.Clamp((byte)g, 0, 255), (byte)Mathf.Clamp((byte)b, 0, 255), o.a);
-        }
     }
 }
